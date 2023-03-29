1f5fd1b23efe54426f8f8057f05e5ec7aa665062
Leonardo Luize <leonardo.luize2@gmail.com>
2023-03-29 09:41:29 -0300

    feat: add eslint configs

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,17 @@
-import "dotenv/config";
-import { z } from "zod";
+import "dotenv/config"
+import { z } from "zod"
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
-  PORT: z.coerce.number().default(3333)
-});
+	NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
+	PORT: z.coerce.number().default(3333)
+})
 
 const _env = envSchema.safeParse(process.env)
 
 if(!_env.success){
-    console.error("⚠️ Invalid environments variables!", _env.error.format())
+	console.error("⚠️ Invalid environments variables!", _env.error.format())
 
-    throw new Error('Invalid environments variable.')
+	throw new Error("Invalid environments variable.")
 }
 
 export const env = _env.data
