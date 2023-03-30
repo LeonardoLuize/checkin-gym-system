c88352a791bff06adb59bc6568b41b51040d4dcb
Leonardo Luize <leonardo.luize2@gmail.com>
2023-03-30 11:49:22 -0300

    feat: global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from "fastify"
+import { ZodError } from "zod"
+import { env } from "./env"
 import { appRoutes } from "./http/routes"
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+	if(error instanceof ZodError){
+		return reply.code(400).send({message: "Validation error", issues: error.format()})
+	}
+
+	if(env.NODE_ENV !== "prod") {
+		console.error(error)
+	} else {
+		// TODO log to external tool
+	}
+
+	return reply.code(500).send({message: "Internal Server Error."})
+})
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
 import { RegisterUseCase } from "@/services/register"
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
+import { UserAlreadyExistsError } from "@/services/errors/user-already-exists"
 
 export async function register (req: FastifyRequest, reply: FastifyReply) {
 	const registerBodySchema = z.object({
@@ -18,7 +19,11 @@ export async function register (req: FastifyRequest, reply: FastifyReply) {
         
 		await registerUseCase.execute({name, email, password})
 	}catch (e) {
-		return reply.status(405).send()
+		if(e instanceof UserAlreadyExistsError){
+			return reply.status(409).send({message: e.message})
+		}
+
+		throw e
 	}
 
 	return reply.status(201).send()
