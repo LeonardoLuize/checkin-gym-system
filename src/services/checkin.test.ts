7cde8300e469ac76f19c9a1e2ff4151e6338f7fd
LeonardoLuize <leonardo.luize2@gmail.com>
2023-04-08 14:59:19 -0300

    feat: add checkins test

--- src/services/checkin.test.ts
+++ src/services/checkin.test.ts
@@ -1,20 +1,81 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory-checkins-repository"
-import { describe,beforeEach, expect, it } from "vitest"
+import { describe,beforeEach, expect, it, vi, afterEach } from "vitest"
 import { CheckInUseCase } from "./checkin"
+import { InMemoryGymsRepository } from "@/repositories/in-memory-gyms-repository"
+import { Decimal } from "@prisma/client/runtime"
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let 	sut: CheckInUseCase
 
 describe("Check-in Use Case", () => {
 	beforeEach(() => {
 		checkInsRepository = new InMemoryCheckInsRepository()
-		sut = new CheckInUseCase(checkInsRepository)
+		gymsRepository = new InMemoryGymsRepository()
+		sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+		vi.useFakeTimers()
+
+		gymsRepository.items.push({
+			id: "gym-01",
+			title: "Academia",
+			description: "teste de academia",
+			phone: "",
+			longitude: new Decimal(0),
+			latitude: new Decimal(0)
+		})
+	})
+
+	afterEach(() => {
+		vi.useRealTimers()
 	})
 
 	it("should be able to create a check-in", async () => {
 		const {checkIn} = await sut.execute({
 			gymId: "gym-01",
 			userId: "user-01",
+			userLatitude: 0,
+			userLongitude: 0,
+		})
+
+		expect(checkIn.id).toEqual(expect.any(String))
+	})
+
+	it("should not be able to check-in twice in the same day", async () => {
+		vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+		await sut.execute({
+			gymId: "gym-01",
+			userId: "user-01",
+			userLatitude: 0,
+			userLongitude: 0,
+		})
+
+		await expect(() => sut.execute({
+			gymId: "gym-01",
+			userId: "user-01",
+			userLatitude: 0,
+			userLongitude: 0,
+		})).rejects.toBeInstanceOf(Error)
+	})
+
+	it("should be able to check-in twice in different days", async () => {
+		vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+		await sut.execute({
+			gymId: "gym-01",
+			userId: "user-01",
+			userLatitude: 0,
+			userLongitude: 0,
+		})
+
+		vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+		const {checkIn} = await sut.execute({
+			gymId: "gym-01",
+			userId: "user-01",
+			userLatitude: 0,
+			userLongitude: 0,
 		})
 
 		expect(checkIn.id).toEqual(expect.any(String))
--- src/services/checkin.ts
+++ src/services/checkin.ts
@@ -2,10 +2,13 @@ import { CheckInsRepository } from "@/repositories/checkins-repository"
 import { UserRepository } from "@/repositories/users-repository"
 import { CheckIn } from "@prisma/client"
 import { ResourceNotFound } from "./errors/resource-not-found"
+import { GymsRepository } from "@/repositories/gyms-repository"
 
 interface CheckInUseCaseRequest {
     userId: string,
     gymId: string,
+	userLatitude: number,
+	userLongitude: number,
 }
 
 interface CheckInUseCaseResponse {
@@ -13,9 +16,21 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase{
-	constructor (private checkInsRepository: CheckInsRepository){}
+	constructor (private checkInsRepository: CheckInsRepository, private gymsRepository: GymsRepository ){}
 
-	async execute({userId, gymId}:CheckInUseCaseRequest ): Promise<CheckInUseCaseResponse>{
+	async execute({userId, gymId, userLatitude, userLongitude}:CheckInUseCaseRequest ): Promise<CheckInUseCaseResponse>{
+		const gym = this.gymsRepository.findById(gymId)
+
+		if(!gym){
+			throw new ResourceNotFound()
+		}
+
+		const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+
+		if(checkInOnSameDate){
+			throw new Error()
+		}
+
 		const checkIn = await this.checkInsRepository.create({
 			gym_id: gymId,
 			user_id: userId,
