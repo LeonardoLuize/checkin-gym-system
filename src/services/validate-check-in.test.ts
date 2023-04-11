53841deca9f921a0dc46f73d9e364eaa4ac20b27
Leonardo Luize <leonardo.luize2@gmail.com>
2023-04-11 15:00:52 -0300

    feat: add validate check in use case

--- src/services/validate-check-in.test.ts
+++ src/services/validate-check-in.test.ts
@@ -1,5 +1,6 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-checkins-repository"
 import { describe,beforeEach, expect, it, vi, afterEach } from "vitest"
+import { LateValidationCheckInError } from "./errors/late-check-in-validation-error"
 import { ResourceNotFound } from "./errors/resource-not-found"
 import { ValidateCheckInUseCase } from "./validate-check-in"
 
@@ -11,11 +12,11 @@ describe("Check-in Use Case", () => {
 		checkInsRepository = new InMemoryCheckInsRepository()
 		sut = new ValidateCheckInUseCase(checkInsRepository)
 
-		//vi.useFakeTimers()
+		vi.useFakeTimers()
 	})
 
 	afterEach(() => {
-		//vi.useRealTimers()
+		vi.useRealTimers()
 	})
 
 	it("should be able to validate a check-in", async () => {
@@ -38,4 +39,20 @@ describe("Check-in Use Case", () => {
 		})).rejects.toBeInstanceOf(ResourceNotFound)
 	})
 
+	it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+		vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+		const checkInCreated = await checkInsRepository.create({
+			gym_id: "gym-01",
+			user_id: "user-01",
+		})
+
+		const twentyOneMinutesInMs = 1000 * 60 * 21
+
+		vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+		await expect(() => sut.execute({
+			checkInId: checkInCreated.id
+		})).rejects.toBeInstanceOf(LateValidationCheckInError)
+	})
 })
--- src/services/validate-check-in.ts
+++ src/services/validate-check-in.ts
@@ -6,6 +6,8 @@ import { GymsRepository } from "@/repositories/gyms-repository"
 import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates"
 import { MaxDistanceError } from "./errors/max-distance-error"
 import { MaxCheckInsError } from "./errors/max-numbers-of-check-ins-error"
+import dayjs from "dayjs"
+import { LateValidationCheckInError } from "./errors/late-check-in-validation-error"
 
 interface ValidateCheckInUseCaseRequest {
     checkInId: string
@@ -27,6 +29,15 @@ export class ValidateCheckInUseCase {
 
 		if(!checkIn){
 			throw new ResourceNotFound()
+		}
+
+		const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+			checkIn.created_at,
+			"minutes",
+		)
+
+		if(distanceInMinutesFromCheckInCreation > 20) {
+			throw new LateValidationCheckInError()
 		}
 
 		checkIn.validated_at = new Date()
