04f325e4470ad108c540ce4488861f80c5f412bc
Leonardo Luize <leonardo.luize2@gmail.com>
2023-05-02 14:23:13 -0300

    fix: update tests with user role

--- src/http/controllers/check-ins/validate.test.ts
+++ src/http/controllers/check-ins/validate.test.ts
@@ -14,7 +14,7 @@ describe("Create Check In (E2E)", () => {
 	})
 
 	it("Should be able to validate a check-in", async () => {
-		const { token } = await createAndAuthenticateUser(app)
+		const { token } = await createAndAuthenticateUser(app, true)
 		const user = await prisma.user.findFirstOrThrow()
 
 		const gym = await prisma.gym.create({
